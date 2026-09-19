a7bd7e6be08811f143064d0c8fc09e3bdb63b065
unknown <alexandru.zlotea02@gmail.com>
2026-09-19 16:50:01 +0300

    slime movement v1

--- Assets/_1 Scripts/PlayerManager.cs
+++ Assets/_1 Scripts/PlayerManager.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] public InputActionReference moveAction;
+    [Header("Player Inputs")]
+    [SerializeField] public InputActionReference moveLeftAction;
+    [SerializeField] public InputActionReference moveRightAction;
     [SerializeField] public InputActionReference jumpAction;
 
+    
+    [Header("Movement")]
+    [SerializeField] private float maxMoveSpeed = 8f;
+    [SerializeField] private float acceleration = 50f;
+    [SerializeField] private float deceleration = 40f;
 
+    private Rigidbody2D playerRigidbody;
+    private Vector2 currentVelocity;
+    private float moveDir;
     private Vector2 moveInput;
     private bool jumpQueued = false;
     private bool isGrounded = false;
@@ -14,7 +25,22 @@ public class PlayerManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveDir = 0;
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float targetSpeedX = currentVelocity.x;
+        float accelRate = Mathf.Abs(targetSpeedX) > 0.01f ? acceleration : deceleration;
+
+        float newVelocityX = Mathf.MoveTowards(
+            playerRigidbody.linearVelocity.x,
+            targetSpeedX,
+            accelRate * Time.fixedDeltaTime
+        );
+
+        playerRigidbody.linearVelocity = new Vector2(newVelocityX, playerRigidbody.linearVelocity.y);
     }
 
     /*
@@ -24,18 +50,36 @@ public class PlayerManager : MonoBehaviour
      */
     private void OnEnable()
     {
-        moveAction.action.Enable();
+        moveLeftAction.action.Enable();
+        moveRightAction.action.Enable();
+        jumpAction.action.Enable();
     }
 
     private void OnDisable()
     {
-        moveAction.action.Disable();
+        moveLeftAction.action.Disable();
+        moveRightAction.action.Disable();
+        jumpAction.action.Disable();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((moveLeftAction.action.ReadValue<float>() != 0) || (moveRightAction.action.ReadValue<float>() != 0))
+            moveDir = moveRightAction.action.ReadValue<float>() - moveLeftAction.action.ReadValue<float>();
+        else 
+            moveDir = 0;
+        HandlePlayerMovement(moveDir);
+    }
+
+    private void HandlePlayerMovement(float moveDir)
+    {
+        if (jumpAction.action.WasPressedThisFrame())
+        {
+            jumpQueued = true;
+        }
+
+        currentVelocity.x = moveDir * maxMoveSpeed;
     }
 }
